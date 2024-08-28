a8840954bcb78363ac25e9fa87b2d02e5b1b6928
KavanKake <Kavin_lokeswaran1407@outlook.com>
2024-08-28 08:47:07 +0200

    Endringer av farger osv

--- index.js
+++ index.js
@@ -6,14 +6,14 @@ $(document).ready(function () {
 			$(".navbar").addClass("solid-nav");
 
 			document.getElementById("logo").style.backgroundImage =
-				"url('./Bilder/Oslo_Kommune_Logo_Transparent.png')";
+				"url('./Bilder/Første\ logo\ desgin\ DD\ Elvebakken_transparent.png')";
 			console.log("solid-nav");
 			//$('#navbar-logo').attr('src', './Bilder/Oslo\ Kommune\ Logo\ Transparent\ Hvit.png'); // Endre til solidt bilde når .navbar har .solid-nav
 		} else {
 			$(".navbar").removeClass("solid-nav");
 			console.log("no-solid-nav");
 			document.getElementById("logo").style.backgroundImage =
-				"url('./Bilder/Oslo_Kommune_Logo_Transparent_Hvit.png')";
+				"url('./Bilder/Første\ logo\ desgin\ DD\ Elvebakken_transparent.png')";
 
 			//$('#navbar-logo').attr('src', './Bilder/Oslo\ Kommune\ Logo\ Transparent.png'); // Endre tilbake til originalt bilde når .navbar ikke har .solid-nav
 		}
